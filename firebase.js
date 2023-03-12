500bbe70ed749760d479629d2e75ddbdaa21220c
Ezequiel De La Rosa <91556738+dEzequiel@users.noreply.github.com>
2023-03-12 07:02:42 +0100

    implementation with firebase backend

---
+++ firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js';
+import { getFirestore, collection, getDocs, getDoc, query, where } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC-RRodeZffkWvt5aiSJKWpivdTPLu7Dcw",
+    authDomain: "fluffy-face.firebaseapp.com",
+    projectId: "fluffy-face",
+    storageBucket: "fluffy-face.appspot.com",
+    messagingSenderId: "900032114742",
+    appId: "1:900032114742:web:a9dafc9c330cd97d0ba897"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get database.
+const db = getFirestore();
+
+// Get brands.
+export const getBrands = () => {
+    return getDocs(collection(db, 'brands'));
+}
+
+// Get brand by name.
+export const getBrandByName = async (name) => {
+    let collectionRef = collection(db, 'brands');
+    const q = query(collectionRef, where('name', '==', name));
+    const entities = await getDocs(q);
+    return entities.docs[0].data()
+}
